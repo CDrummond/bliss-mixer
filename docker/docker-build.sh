e82b119270fbd1b85d91c9ca641413f02e7e8459
Craig Drummond <craig.p.drummond@gmail.com>
2022-02-21 21:46:06 +0000

    Change folder structure to match what will be used.

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -5,11 +5,10 @@ set -eux
 uname -a
 DESTDIR=/src/releases
 
-mkdir -p $DESTDIR/i386-linux
-rm -f $DESTDIR/i386-linux/*
-
-mkdir -p $DESTDIR/arm-linux
-rm -f $DESTDIR/arm-linux/*
+for d in armhf-linux arm-linux i386-linux x86_64-linux ; do
+    mkdir -p $DESTDIR/$d
+    rm -f $DESTDIR/$d/*
+done
 
 function build {
 	echo Building for $1 to $3...
@@ -18,11 +17,10 @@ function build {
 		cargo build --release --target $1
 	fi
 
-	$2 /build/$1/release/bliss-mixer \
-		&& cp /build/$1/release/bliss-mixer $DESTDIR/$3
+	$2 /build/$1/release/bliss-mixer && cp /build/$1/release/bliss-mixer $DESTDIR/$3
 }
 
-build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip arm-linux/bliss-mixer-hf
-build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip arm-linux/bliss-mixer-aarch64
-build x86_64-unknown-linux-musl strip i386-linux/bliss-mixer-x86_64
+build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip armhf-linux/bliss-mixer
+build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip arm-linux/bliss-mixer
+build x86_64-unknown-linux-musl strip x86_64-linux/bliss-mixer
 build i686-unknown-linux-musl strip i386-linux/bliss-mixer
