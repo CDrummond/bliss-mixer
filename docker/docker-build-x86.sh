126e5303c3214a51829353a928650700d94544d4
CDrummond <craig.p.drummond@gmail.com>
2025-09-24 21:47:14 +0100

    Use docker for all Linux builds and link to musl

--- docker/docker-build-x86.sh
+++ docker/docker-build-x86.sh
@@ -5,11 +5,6 @@ set -eux
 uname -a
 DESTDIR=/src/releases
 
-for d in armhf-linux aarch64-linux ; do
-    mkdir -p $DESTDIR/$d
-    rm -f $DESTDIR/$d/*
-done
-
 function build {
     echo Building for $1 to $3...
 
@@ -20,6 +15,5 @@ function build {
     $2 /build/$1/release/bliss-mixer && cp /build/$1/release/bliss-mixer $DESTDIR/$3
 }
 
-build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip armhf-linux/bliss-mixer
-build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip aarch64-linux/bliss-mixer
+build x86_64-unknown-linux-musl strip bliss-mixer
 
