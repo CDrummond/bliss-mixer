5607021b92376e3ff53f256b2e3b24f7ff9bb50d
CDrummond <craig.p.drummond@gmail.com>
2025-09-24 22:10:51 +0100

    Package as per analyser

--- docker/docker-build-arm.sh
+++ docker/docker-build-arm.sh
@@ -5,10 +5,8 @@ set -eux
 uname -a
 DESTDIR=/src/releases
 
-for d in armhf-linux aarch64-linux ; do
-    mkdir -p $DESTDIR/$d
-    rm -f $DESTDIR/$d/*
-done
+mkdir -p $DESTDIR/bin
+rm -rf $DESTDIR/bin/*
 
 function build {
     echo Building for $1 to $3...
@@ -20,6 +18,6 @@ function build {
     $2 /build/$1/release/bliss-mixer && cp /build/$1/release/bliss-mixer $DESTDIR/$3
 }
 
-build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip armhf-linux/bliss-mixer
-build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip aarch64-linux/bliss-mixer
-
+build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip bin/bliss-mixer-armhf
+build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip bin/bliss-mixer-aarch64
+cp scripts/bliss-mixer-arm $DESTDIR/bliss-mixer
